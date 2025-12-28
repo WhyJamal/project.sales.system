488048ae3031fd972e38c69229c0cfed0826ef71
Thanos-in <sayyodbee2006@gmail.com>
2025-12-28 14:45:53 +0500

    Fix: children rendered above background layers with relative z-10 in Container

--- frontend.base.r/src/views/pages/products/ProductsView.tsx
+++ frontend.base.r/src/views/pages/products/ProductsView.tsx
@@ -6,7 +6,8 @@ import { Container } from "@/shared/components/container";
 const ProductsView: React.FC = () => {
   return (
     <Container className="flex py-16">
-      <div className="max-w-5xl mx-auto px-4">
+      <div className="max-w-5xl mx-auto px-4 z-10">
+        
         <h2 className="text-white text-xl font-semibold mb-10">Приложения</h2>
 
         <div className="space-y-6">
