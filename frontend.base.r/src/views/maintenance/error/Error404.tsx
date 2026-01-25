66bf52a70f2f3d982be5019637580a4297180c58
Thanos-in <sayyodbee2006@gmail.com>
2026-01-25 08:55:59 +0500

    style: improve 404 page mobile responsiveness and optimize background image
    - Adjust layout and typography for better mobile display
    - Center content on small screens while keeping desktop layout unchanged
    - Replace background image from PNG to WebP for better performance

--- frontend.base.r/src/views/maintenance/error/Error404.tsx
+++ frontend.base.r/src/views/maintenance/error/Error404.tsx
@@ -5,13 +5,21 @@ import { motion } from "framer-motion";
 const NotFoundPage: React.FC = () => {
   return (
     <div
-      className="flex flex-col items-start justify-start h-screen text-left text-white p-10 md:p-20"
+      className="
+        flex flex-col
+        items-start
+        justify-start
+        h-screen
+        text-left
+        text-white
+        p-11 sm:p-10 md:p-20
+      "
       style={{
-        background: `#00AFF9 url(/images/Unplugged.png) center/cover no-repeat`,
+        background: `#00AFF9 url(/images/Unplugged.webp) center/cover no-repeat`,
       }}
     >
       <motion.h1
-        className="text-5xl sm:text-6xl md:text-7xl font-bold mb-4"
+        className="text-4xl sm:text-6xl md:text-7xl font-bold mb-4"
         initial={{ x: -500, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ type: "spring", stiffness: 70, delay: 0.2 }}
@@ -20,7 +28,7 @@ const NotFoundPage: React.FC = () => {
       </motion.h1>
 
       <motion.p
-        className="text-xl sm:text-2xl md:text-2xl mb-6"
+        className="text-lg sm:text-2xl mb-6 max-w-md"
         initial={{ x: -500, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ type: "spring", stiffness: 70, delay: 0.4 }}
@@ -35,7 +43,12 @@ const NotFoundPage: React.FC = () => {
       >
         <Link
           to="/"
-          className="px-4 sm:px-6 py-2 sm:py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition text-sm sm:text-base"
+          className="
+            px-6 py-3
+            bg-blue-600 text-white rounded
+            hover:bg-blue-700 transition
+            text-base
+          "
         >
           На главную
         </Link>
