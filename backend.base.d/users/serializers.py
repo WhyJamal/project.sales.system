e86e52dc123674ecb0396d442386108f4d70ec5a
Thanos-in <sayyodbee2006@gmail.com>
2026-01-27 12:52:49 +0500

    Add avatar field to UserSerializer and optimize organization data

--- backend.base.d/users/serializers.py
+++ backend.base.d/users/serializers.py
@@ -1,19 +1,35 @@
 from rest_framework import serializers
+
 from .models import CustomUser
+from django.conf import settings
 
 class UserSerializer(serializers.ModelSerializer):
-    organization_id = serializers.IntegerField(source="organization.id", read_only=True)
-    organization_name = serializers.CharField(source="organization.name", read_only=True)    
-    organization_url = serializers.CharField(source="organization.url", read_only=True)    
-    
+    organization = serializers.SerializerMethodField()
+    avatar = serializers.SerializerMethodField()  
+
     class Meta:
         model = CustomUser
         fields = [
-            'id', 'username', 'email', 'phone_number',
-            'organization_id', 'organization_name', 'bio',
-            'organization_url', 'password'
+            'id', 'username', 'email', 'phone_number', 'bio',
+            'password', 'avatar', 'organization'
         ]
         extra_kwargs = {'password': {'write_only': True}}
+
+    def get_organization(self, obj):
+        org = obj.organization
+        if not org:
+            return None
+        return {
+            "id": org.id,
+            "name": org.name,
+            "url": org.url,
+            "products": get_org_products(org),
+        }
+
+    def get_avatar(self, obj):
+        if obj.avatar:  
+            return f"{settings.SITE_URL}{obj.avatar.url}"
+        return None  
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
